a684d45e50fa7ef03277b1f1229c09e4b3ec7dc1
Shaikh Afroz <shaikhafroz@geekyants.com>
2023-03-06 19:31:57 +0530

    ADD: PointerValue Label

--- example/lib/pointer_value_label.dart
+++ example/lib/pointer_value_label.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:geekyants_flutter_gauges/gauges.dart';
 
-class PointerValueLabel extends StatelessWidget {
+class PointerValueLabel extends StatefulWidget {
   const PointerValueLabel({super.key});
 
   @override
+  State<PointerValueLabel> createState() => _PointerValueLabelState();
+}
+
+class _PointerValueLabelState extends State<PointerValueLabel> {
+  @override
   Widget build(BuildContext context) {
     return const LinearGauge(
-      start: 0,
-      value: 50,
-      end: 100,
+      value: 30,
       pointer: Pointer(
-        color: Colors.blue,
-        shape: PointerShape.triangle,
-        quaterTurns: QuaterTurns.two,
+        color: Colors.red,
+        width: 70,
+        height: 390,
+        shape: PointerShape.rectangle,
+        quaterTurns: QuaterTurns.three,
         labelStyle: TextStyle(
-          color: Colors.red,
+          color: Colors.black,
           fontSize: 10.0,
           fontWeight: FontWeight.bold,
         ),
       ),
+      steps: 1,
+      start: 0,
+      end: 50,
+      rulers: RulerStyle(rulerPosition: RulerPosition.top),
     );
   }
 }
