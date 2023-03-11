2c1c04164978eff7cdd8b79f9f7a32f2d3a5a138
hasnentai <hasnen712@live.com>
2023-03-11 23:51:07 +0530

    Added animation to triangle

--- example/lib/gauge_vertical.dart
+++ example/lib/gauge_vertical.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geekyants_flutter_gauges/gauges.dart';
+import 'package:geekyants_flutter_gauges/linear_gauge/value_bar/value_bar.dart';
 
 class MyVerticalGauge extends StatefulWidget {
   const MyVerticalGauge({super.key});
@@ -14,8 +15,18 @@ class _MyVerticalGaugeState extends State<MyVerticalGauge> {
     return Scaffold(
       body: Center(
         child: LinearGauge(
-          gaugeOrientation: GaugeOrientation.vertical,
-          rulers: const RulerStyle(rulerPosition: RulerPosition.left),
+          start: 100,
+          end: 200,
+          pointers: const [Pointer(shape: PointerShape.triangle, value: 200)],
+          valueBar: [
+            ValueBar(
+              value: 200,
+            ),
+          ],
+          gaugeOrientation: GaugeOrientation.horizontal,
+          rulers: const RulerStyle(
+            rulerPosition: RulerPosition.bottom,
+          ),
         ),
       ),
     );
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:example/gauge_vertical.dart';
 import 'package:flutter/material.dart';
-import 'package:geekyants_flutter_gauges/gauges.dart';
-import 'package:geekyants_flutter_gauges/linear_gauge/value_bar/value_bar.dart';
 
 void main() {
   runApp(
@@ -22,6 +20,6 @@ class MyGaugeExample extends StatefulWidget {
 class _MyGaugeExampleState extends State<MyGaugeExample> {
   @override
   Widget build(BuildContext context) {
-    return MyVerticalGauge();
+    return const MyVerticalGauge();
   }
 }
