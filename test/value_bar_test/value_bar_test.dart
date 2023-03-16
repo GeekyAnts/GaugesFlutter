1d6717bbd3c1f5517141bac4d0a124467fa680a8
SahilSharma2710 <s90sahil90@gmail.com>
2023-03-16 18:16:31 +0530

    goldens for valur bar added

--- test/value_bar_test/value_bar_test.dart
+++ test/value_bar_test/value_bar_test.dart
@@ -6,37 +6,19 @@ import 'package:golden_toolkit/golden_toolkit.dart';
 import 'test_cases.dart';
 
 void main() {
-  group("Test the Pointer Functionality", () {
+  group("Test the ValueBar Functionality", () {
     final testCases = allTests;
 
     for (final testCase in testCases) {
       testGoldens(testCase['Do'] as String, (tester) async {
         await tester.pumpWidgetBuilder(
-          MyPointerTestLinearGauge(
-            thickness: testCase['gaugeThickness'] as double?,
-            rulerPosition: testCase['rulerPosition'] as RulerPosition?,
-            pointer: testCase['pointer'] as Pointer?,
+          MyValuBarTestLinearGauge(
             gaugeOrientation: testCase['gaugeOrientation'] as GaugeOrientation?,
-          ),
-          surfaceSize: const Size(1200, 900),
-        );
-        await screenMatchesGolden(tester, testCase['name'] as String);
-      });
-    }
-  });
-
-  group("Test the Pointer Functionality with extendLinearGauge", () {
-    final testCases = extendLinearGaugePointersAlltests;
-
-    for (final testCase in testCases) {
-      testGoldens(testCase['Do'] as String, (tester) async {
-        await tester.pumpWidgetBuilder(
-          MyPointerTestLinearGauge(
+            linearGaugeBoxDecoration: testCase['linearGaugeBoxDecoration']
+                as LinearGaugeBoxDecoration?,
+            valueBar: testCase['valueBar'] as List<ValueBar>?,
             rulers: testCase['rulers'] as RulerStyle?,
             extendLinearGauge: testCase['extendLinearGauge'] as double?,
-            rulerPosition: testCase['rulerPosition'] as RulerPosition?,
-            pointer: testCase['pointer'] as Pointer?,
-            gaugeOrientation: testCase['gaugeOrientation'] as GaugeOrientation?,
           ),
           surfaceSize: const Size(1200, 900),
         );
@@ -46,23 +28,20 @@ void main() {
   });
 }
 
-class MyPointerTestLinearGauge extends StatelessWidget {
-  final RulerPosition? rulerPosition;
-  final double? thickness;
+class MyValuBarTestLinearGauge extends StatelessWidget {
   final double? extendLinearGauge;
   final GaugeOrientation? gaugeOrientation;
-  final Pointer? pointer;
   final RulerStyle? rulers;
+  final List<ValueBar>? valueBar;
+  final LinearGaugeBoxDecoration? linearGaugeBoxDecoration;
 
-  const MyPointerTestLinearGauge({
-    super.key,
-    this.extendLinearGauge,
-    this.rulers,
-    this.pointer,
-    this.rulerPosition,
-    this.gaugeOrientation,
-    this.thickness,
-  });
+  const MyValuBarTestLinearGauge(
+      {super.key,
+      this.extendLinearGauge,
+      this.rulers,
+      this.gaugeOrientation,
+      this.valueBar,
+      this.linearGaugeBoxDecoration});
 
   @override
   Widget build(BuildContext context) {
@@ -76,11 +55,15 @@ class MyPointerTestLinearGauge extends StatelessWidget {
         ),
         body: Center(
           child: LinearGauge(
-            linearGaugeBoxDecoration:
-                LinearGaugeBoxDecoration(thickness: thickness ?? 4),
+            linearGaugeBoxDecoration: linearGaugeBoxDecoration ??
+                const LinearGaugeBoxDecoration(
+                  thickness: 4.0,
+                  linearGaugeValueColor: Colors.blue,
+                  backgroundColor: Colors.grey,
+                ),
             extendLinearGauge: extendLinearGauge ?? 0,
             gaugeOrientation: gaugeOrientation ?? GaugeOrientation.horizontal,
-            pointers: [pointer!],
+            valueBar: valueBar,
             rulers: rulers ??
                 const RulerStyle(
                   rulerPosition: RulerPosition.center,
