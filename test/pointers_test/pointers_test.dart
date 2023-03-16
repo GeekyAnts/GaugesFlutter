adcd75418ef2d7da5d11f784c1fc495998a07cd8
SahilSharma2710 <s90sahil90@gmail.com>
2023-03-16 13:46:20 +0530

    goldens for extendLinearGauge for pointers added

--- test/pointers_test/pointers_test.dart
+++ test/pointers_test/pointers_test.dart
@@ -24,15 +24,37 @@ void main() {
       });
     }
   });
+
+  group("Test the Pointer Functionality with extendLinearGauge", () {
+    final testCases = extendLinearGaugePointerTests;
+
+    for (final testCase in testCases) {
+      testGoldens(testCase['Do'] as String, (tester) async {
+        await tester.pumpWidgetBuilder(
+          MyPointerTestLinearGauge(
+            extendLinearGauge: testCase['extendLinearGauge'] as double?,
+            rulerPosition: testCase['rulerPosition'] as RulerPosition?,
+            pointer: testCase['pointer'] as Pointer?,
+            gaugeOrientation: testCase['gaugeOrientation'] as GaugeOrientation?,
+          ),
+          surfaceSize: const Size(1200, 900),
+        );
+        await screenMatchesGolden(tester, testCase['name'] as String);
+      });
+    }
+  });
 }
 
 class MyPointerTestLinearGauge extends StatelessWidget {
   final RulerPosition? rulerPosition;
   final double? thickness;
+  final double? extendLinearGauge;
+
   final GaugeOrientation? gaugeOrientation;
   final Pointer? pointer;
   const MyPointerTestLinearGauge({
     super.key,
+    this.extendLinearGauge,
     this.pointer,
     this.rulerPosition,
     this.gaugeOrientation,
@@ -53,6 +75,7 @@ class MyPointerTestLinearGauge extends StatelessWidget {
           child: LinearGauge(
             linearGaugeBoxDecoration:
                 LinearGaugeBoxDecoration(thickness: thickness ?? 4),
+            extendLinearGauge: extendLinearGauge ?? 0,
             gaugeOrientation: gaugeOrientation ?? GaugeOrientation.horizontal,
             pointers: [pointer!],
             rulers: RulerStyle(
