337156a1e2d1f00fd73d8c515dbfbe8e967bd7f7
Afroz-Shaikh <afrozshaikh725@gmail.com>
2023-03-21 11:23:36 +0530

    WIP

--- example/lib/gauge_vertical.dart
+++ example/lib/gauge_vertical.dart
@@ -13,19 +13,27 @@ class _MyVerticalGaugeState extends State<MyVerticalGauge> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: LinearGauge(
-          enableAnimation: true,
-          valueBar: [
-            ValueBar(
-              value: 30,
-              offset: 50,
-              position: ValueBarPosition.top,
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: LinearGauge(
+            value: 100,
+            linearGaugeBoxDecoration: const LinearGaugeBoxDecoration(
+                thickness: 30,
+                borderRadius: 10,
+                linearGradient: LinearGradient(
+                    colors: [Color(0xff4f76fc), Color(0xfff882fc)])),
+            // enableAnimation: true,
+            gaugeOrientation: GaugeOrientation.horizontal,
+            rulers: const RulerStyle(
+              primaryRulersWidth: 10,
+              primaryRulersHeight: 30,
+              primaryRulerColor: Color(0xff310072),
+              inverseRulers: false,
+              showLabel: false,
+              showSecondaryRulers: false,
+              showPrimaryRulers: true,
+              rulerPosition: RulerPosition.center,
             ),
-          ],
-          gaugeOrientation: GaugeOrientation.horizontal,
-          rulers: const RulerStyle(
-            inverseRulers: false,
-            rulerPosition: RulerPosition.top,
           ),
         ),
       ),
