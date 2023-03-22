bde1a59dbcc679fb81f3ea1bfa2aa6ab05bc6b7a
Afroz-Shaikh <afrozshaikh725@gmail.com>
2023-03-22 17:18:07 +0530

    Add: ShowCase App

--- lib/linear_gauge/linear_gauge_painter.dart
+++ lib/linear_gauge/linear_gauge_painter.dart
@@ -1587,6 +1587,7 @@ class RenderLinearGauge extends RenderBox {
 
   @override
   Size computeDryLayout(BoxConstraints constraints) {
+<<<<<<< HEAD
     double parentWidgetSize;
 
     final double actualParentWidth = constraints.maxWidth;
@@ -1605,6 +1606,17 @@ class RenderLinearGauge extends RenderBox {
     }
 
     return constraints.constrain(_axisActualSize);
+=======
+    final desiredWidth = getGaugeOrientation == GaugeOrientation.vertical
+        ? constraints.minWidth
+        : constraints.maxWidth;
+    final desiredHeight = getGaugeOrientation == GaugeOrientation.horizontal
+        ? constraints.minHeight
+        : constraints.maxHeight;
+    final desiredSize = Size(desiredWidth, desiredHeight);
+    print(desiredSize);
+    return constraints.constrain(desiredSize);
+>>>>>>> c1fabb2 (Add: ShowCase App)
   }
 
   @override
