610337b0445beda10d2bd1f7fb0665bfcbd1676f
Afroz-Shaikh <afrozshaikh725@gmail.com>
2023-05-24 11:40:46 +0530

    ADD: radial Gauge

--- test/widget_test/rulers_test.dart
+++ test/widget_test/rulers_test.dart
@@ -3,23 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:geekyants_flutter_gauges/geekyants_flutter_gauges.dart';
 
 void main() {
-<<<<<<< HEAD
   var linearGauge = MaterialApp(
     home: Scaffold(
       body: LinearGauge(
         start: 0,
         end: 100,
-        rulers: const RulerStyle(
+        rulers: RulerStyle(
           rulerPosition: RulerPosition.bottom,
         ),
       ),
-=======
-  LinearGauge linearGauge = LinearGauge(
-    start: 0,
-    end: 100,
-    rulers: RulerStyle(
-      rulerPosition: RulerPosition.bottom,
->>>>>>> 3c83939 (Fix: goldens updated)
     ),
   );
   testWidgets('Verify the default start and end', (tester) async {
